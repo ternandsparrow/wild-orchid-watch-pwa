fac28a5077697c1c39f582d85cb5eb4719e964ea
Tom Saleeba <tom.saleeba@gmail.com>
2019-08-28 22:05:30 +0930

    chore: fix incorrect path during build

--- scripts/build-and-sentry-release.sh
+++ scripts/build-and-sentry-release.sh
@@ -2,15 +2,16 @@
 # builds our app and performs a Sentry.io release
 set -euxo pipefail
 cd `dirname "$0"`
+thisDir=`pwd`
 
 : ${SENTRY_AUTH_TOKEN:?}
 : ${SENTRY_ORG:?}
 : ${SENTRY_PROJECT:?}
 
-./gen-version.sh
+$thisDir/gen-version.sh
 export VUE_APP_VERSION=`node -e "ver = require('./current-version.js'); console.log(ver)"`
 
-./add-dev-markings-if-required.sh pre-build
+$thisDir/add-dev-markings-if-required.sh pre-build
 
 cd ..
 export DO_SENTRY_RELEASE=true
@@ -18,4 +19,4 @@ export DO_SENTRY_RELEASE=true
 # see vue-config/config.production.js for details
 yarn build
 
-./add-dev-markings-if-required.sh post-build
+$thisDir/add-dev-markings-if-required.sh post-build
