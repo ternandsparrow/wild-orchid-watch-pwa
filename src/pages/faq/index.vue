574e2d14b3d47740f80f0fcea90d80c7901bec21
Andrew Tokmakoff <tokmakoff@computer.org>
2019-09-30 17:36:57 +0930

    feat: enabled skeletons of FAQ and Science pages, to be further filled-in.

--- src/pages/faq/index.vue
+++ src/pages/faq/index.vue
@@ -14,17 +14,87 @@
 
       <v-ons-list-item>
         <div class="center">
-          <span class="list-item__title">[#1]: How do I.... ?</span
-          ><span class="list-item__subtitle">It is simplest to...</span>
+          <span class="list-item__title"
+            >[#1]: Do I need to be super-accurate on all the questions you are
+            asking?</span
+          ><span class="list-item__subtitle">Well, it goes like this...</span>
         </div>
       </v-ons-list-item>
       <v-ons-list-item>
         <div class="center">
           <span class="list-item__title"
-            >[#2]: Should I always collect... ?</span
-          ><span class="list-item__subtitle"
-            >No, it is only necessary to..</span
-          >
+            >[#2]: Who will make definitive Identifications of the orchids that
+            are sighted?</span
+          ><span class="list-item__subtitle">Well, it goes like this...</span>
+        </div>
+      </v-ons-list-item>
+      <v-ons-list-item>
+        <div class="center">
+          <span class="list-item__title"
+            >[#3]: What should I use for a field name if I don't really
+            know??</span
+          ><span class="list-item__subtitle">Well, it goes like this...</span>
+        </div>
+      </v-ons-list-item>
+      <v-ons-list-item>
+        <div class="center">
+          <span class="list-item__title"
+            >[#4]: Do I need to take all of the photos?</span
+          ><span class="list-item__subtitle">Well, it goes like this...</span>
+        </div>
+      </v-ons-list-item>
+      <v-ons-list-item>
+        <div class="center">
+          <span class="list-item__title"
+            >[#5]: If something goes wrong in the app, how do I report it?</span
+          ><span class="list-item__subtitle">Well, it goes like this...</span>
+        </div>
+      </v-ons-list-item>
+      <v-ons-list-item>
+        <div class="center">
+          <span class="list-item__title"
+            >[#6]: Will the app use up all my data and phone storage?</span
+          ><span class="list-item__subtitle">Well, it goes like this...</span>
+        </div>
+      </v-ons-list-item>
+      <v-ons-list-item>
+        <div class="center">
+          <span class="list-item__title"
+            >[#7]: Will the pictures I take be stored on my phone also, for
+            me?</span
+          ><span class="list-item__subtitle">Well, it goes like this...</span>
+        </div>
+      </v-ons-list-item>
+      <v-ons-list-item>
+        <div class="center">
+          <span class="list-item__title"
+            >[#8]: Can I use an external camera to take the photos and the app
+            to submit them?</span
+          ><span class="list-item__subtitle">Well, it goes like this...</span>
+        </div>
+      </v-ons-list-item>
+      <v-ons-list-item>
+        <div class="center">
+          <span class="list-item__title"
+            >[#9]: Can I submit a sighting using photos that I took
+            before?</span
+          ><span class="list-item__subtitle">Well, it goes like this...</span>
+        </div>
+      </v-ons-list-item>
+      <v-ons-list-item>
+        <div class="center">
+          <span class="list-item__title"
+            >[#10]: Will the orchids be "safe" when I make sightings with their
+            location?</span
+          ><span class="list-item__subtitle">Well, it goes like this...</span>
+        </div>
+      </v-ons-list-item>
+      <v-ons-list-item>
+        <div class="center">
+          <span class="list-item__title"
+            >[#11]: How will researchers gain access to the data being
+            collected?</span
+          ><span class="list-item__subtitle">Well, it goes like this...</span>
         </div>
       </v-ons-list-item>
     </v-ons-list>
