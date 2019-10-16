c0679aefc122d4a49d45e07dcb4399147c2da1ea
Tom Saleeba <tom.saleeba@gmail.com>
2019-10-16 15:19:26 +1030

    chore: remove firebase auth specific blacklist entries from workbox precache as we aren't using firebase auth

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,14 +18,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || [])
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
 
 // Redirect to index.html if sw cannot find matching route
-workbox.routing.registerNavigationRoute('/index.html', {
-  /* Do not redirect routes used by firebase auth  */
-  blacklist: [
-    new RegExp('/__/auth/handler'),
-    new RegExp('/__/auth/iframe'),
-    new RegExp('/.well-known'),
-  ],
-})
+workbox.routing.registerNavigationRoute('/index.html', {})
 
 workbox.routing.registerRoute(
   /^https:\/\/fonts/,
