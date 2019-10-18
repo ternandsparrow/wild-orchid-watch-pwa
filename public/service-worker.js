ed89e017c6e839ec39f686113f96a2c1de1b0d1d
Tom Saleeba <tom.saleeba@gmail.com>
2019-10-18 11:02:59 +1030

    fix: WOW-117 make sure cache-busting is honoured by service worker, remove inat Rails cache as we don't make calls to inat (that need caching)

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,7 +7,8 @@ console.debug(`Using iNat URL = ${inatUrl}`)
 const inatStaticUrl = VUE_APP_INAT_STATIC_BASE_URL
 console.debug(`Using iNat static URL = ${inatStaticUrl}`)
 
-workbox.core.setCacheNameDetails({ prefix: 'wildorchidwatch' })
+const cachePrefix = 'wildorchidwatch'
+workbox.core.setCacheNameDetails({ prefix: cachePrefix })
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -32,7 +33,14 @@ workbox.routing.registerRoute(
 // never cache requests for API auth tokens.
 // we don't need to worry about the iNat token as that is a POST
 workbox.routing.registerRoute(
-  `${inatUrl}/users/api_token`,
+  new RegExp(`^${inatUrl}/users/api_token.*`),
+  new workbox.strategies.NetworkOnly(),
+  'GET',
+)
+
+// Don't let service worker cache anything that's cache busting
+workbox.routing.registerRoute(
+  new RegExp(`^${apiUrl}/.*cache-bust.*`),
   new workbox.strategies.NetworkOnly(),
   'GET',
 )
@@ -40,32 +48,31 @@ workbox.routing.registerRoute(
 workbox.routing.registerRoute(
   new RegExp(`^${apiUrl}/.*`),
   new workbox.strategies.NetworkFirst({
-    networkTimeoutSeconds: 10,
+    networkTimeoutSeconds: 5,
+    cacheName: cachePrefix + '-inat-api',
+    plugins: [],
   }),
   'GET',
 )
 
-// TODO if we can't cache images, do we even want caching?
+// At the time of writing this, iNat (the Rails app) and the production CDN do
+// NOT return CORS headers on images, so we can't cache them. This code is here
+// in the hope that we get those headers at some stage, but in the interim, the
+// browser disk cache seems to do a passable job. Actually we can cache opaque
+// responses but it's a terrible idea, see
+// https://cloudfour.com/thinks/when-7-kb-equals-7-mb/ for more details.
 workbox.routing.registerRoute(
-  new RegExp(`^${inatUrl}/.*`),
+  new RegExp(`^${inatStaticUrl}/.*`),
   new workbox.strategies.CacheFirst({
+    cacheName: cachePrefix + '-inat-static',
     plugins: [
       new workbox.expiration.Plugin({
-        maxAgeSeconds: 1 * 60 * 60, // 1 hour
+        maxAgeSeconds: 1 * 24 * 60 * 60, // 1 day
       }),
     ],
   }),
   'GET',
 )
-
-const isStaticServerDifferent = inatUrl !== inatStaticUrl
-if (isStaticServerDifferent) {
-  workbox.routing.registerRoute(
-    new RegExp(`^${inatStaticUrl}/.*`),
-    new workbox.strategies.CacheFirst(),
-    'GET',
-  )
-}
 
 addEventListener('message', messageEvent => {
   if (messageEvent.data === 'skipWaiting') return self.skipWaiting()
