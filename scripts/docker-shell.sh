a63ba7ae2958c72d7865144e26a47803f3bb9242
Tom Saleeba <tom.saleeba@gmail.com>
2020-03-23 10:21:10 +1100

    chore: add a script to sync to the VM to make working with a remote dev server easier

--- scripts/docker-shell.sh
+++ scripts/docker-shell.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 # drops to a shell in a docker container that has the tools you need to run
 # this project in a pinch on a VM that you haven't installed npm, yarn, etc
-# onto
+# onto. Check out the rsync-up.sh script too.
+set -euo pipefail
+cd `dirname "$0"/..`
 
 thePort=${PORT:-8080}
 
 docker run \
   --rm \
   -it \
+  --name=wow-shell \
   --user=$(id -u) \
   -v $(pwd):/app \
   -p $thePort:8080 \
---
+++ scripts/rsync-up.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# copies your working directory to some other computer.
+# Intended to be used with the docker-shell.sh script when you're tight on
+# internet quota. Start a VM in the cloud, use the docker-shell.sh to quickly
+# get a dev server running. Continue working on your local machine and use this
+# script to rsync to the VM where the dev server will see the changes to the
+# volume mounted files and rebuild. Simples.
+set -euo pipefail
+cd `dirname "$0"`/..
+
+sshTargetSpec=${1:?first param must be rsync target spec, like user@some.host:/path/to/workspace}
+
+compressParam="-zz" # rsync tells me to use this over --compress
+
+rsync \
+  --archive \
+  --partial \
+  --progress \
+  $compressParam \
+  . \
+  --exclude=.git/ \
+  --exclude=node_modules/ \
+  --exclude=dist/ \
+  --delete \
+  $sshTargetSpec
