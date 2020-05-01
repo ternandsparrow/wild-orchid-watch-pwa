3630ba0af7274860c77863536cbcac863fdcb134
Tom Saleeba <tom.saleeba@gmail.com>
2020-05-01 20:03:14 +1000

    feat: add updated orchid science text from GG

--- src/pages/orchid-science/index.vue
+++ src/pages/orchid-science/index.vue
@@ -1,15 +1,11 @@
 <template>
   <menu-wrapper title="Orchid Science">
     <v-ons-list>
-      <v-ons-list-header
-        >This page will contain information on the science which is enabled by
-        the collected Observations..
-      </v-ons-list-header>
       <ons-list-item>
         <label class="center">
-          Wild Orchid Watch (WOW) is a national citizen science, orchid data
-          collection project. An online platform incorporating an app, website
-          and database to enable researchers, native orchid enthusiasts and
+          Wild Orchid Watch (WOW) is a national citizen science project designed
+          for orchid data collection. An online platform incorporating an app,
+          website and database enable researchers, native orchid enthusiasts and
           citizen scientists to upload and identify orchid observations. The WOW
           app will guide users to make a series of observations in the field,
           collecting information designed to be of maximum relevance to
@@ -21,13 +17,13 @@
           Australia has the world's most diverse terrestrial orchid flora.
           However, our native orchids are highly susceptible to environmental
           change and account for 17% of nationally threatened plant species.
-          Recent studies highlight climate change, florivory (animals eating the
-          flowers), altered fire regimes and pollination deficits as stressors
-          that are contributing to orchid decline, and many questions on their
-          taxonomy and ecology remain unanswered. Orchids are often cryptic,
-          population sizes and short flowering times vary with season, and they
-          are sporadically distributed across Australia, making it difficult to
-          obtain data using traditional methods.
+          Where orchids have survived outright destruction of their natural
+          habitats, climate change, altered fire and grazing regimes, and
+          pollination deficits contribute to population declines. Many questions
+          about orchid taxonomy, ecology and conservation remain unanswered.
+          Orchids are often cryptic, and differences in emergence with season
+          combined with short flowering times make it difficult to obtain data
+          using traditional methods.
         </label>
       </ons-list-item>
       <ons-list-item>
@@ -35,12 +31,13 @@
           This program builds on the growing momentum for citizen science in
           Australia, harnessing the knowledge and expertise of orchid
           enthusiasts who are keen to get involved. Data collected by citizen
-          scientists using the WOW app will allow researchers to understand the
-          value of orchids as indicators of environmental change, as well as
-          provide fundamental information on orchid distribution, abundance, and
-          phenology (life cycle events; emergence, flowering, fruiting), all of
-          which are critical to understanding their complex taxonomy and
-          assigning conservation status.
+          scientists using the WOW app will add to historical records to allow
+          researchers to understand the value of orchids as indicators of
+          environmental change, as well as provide fundamental information on
+          orchid distribution, abundance, and phenology (life cycle events;
+          emergence, flowering, fruiting) and how they are changing. This new
+          information is critical to better understanding the status and trend
+          of orchids and assessing their conservation status.
         </label>
       </ons-list-item>
     </v-ons-list>
